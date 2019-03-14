a73ca33d31846775cad2504cb7f4c5906cca3cdb
Saereth <guiken@gmail.com>
2019-03-13 23:29:04 -0400

    upgrade gradle and build to 1.12

---
+++ src/main/java/com/insane/switchescontinued/constants.java
@@ -0,0 +1,32 @@
+package com.insane.switchescontinued;
+
+import net.minecraft.util.ResourceLocation;
+
+import java.util.Locale;
+
+public class Constants {
+	public static class Localizations {
+		public static class Text {
+		}
+
+		public static class Tooltips {
+		}
+	}
+
+	public static class Misc {
+	}
+
+	public static class Resource {
+	}
+
+	public static class Mod {
+		public static final String MODID   = "simpleswitches";
+		public static final String DOMAIN  = MODID.toLowerCase(Locale.ENGLISH) + ":";
+		public static final String NAME    = "SimpleSwitches";
+		public static final String VERSION = "1";
+	}
+
+	public static class NBT {
+	}
+
+}
